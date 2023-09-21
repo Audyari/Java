84905b18a1db13823954e86dbedfff9d3ef18ce7
Audyari <audy123ari@gmail.com>
2023-09-21 08:31:45 +0700

    Arrays 8.31

---
+++ Mahasiswa.java
@@ -0,0 +1,39 @@
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Mahasiswa {
+    public static void main(String[] args) {
+     
+        int[] test = new int[5];
+        test[0] = 1;
+        System.out.println(test[0]);
+
+    }
+}
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        
+        int ans = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+        
+        return ans;
+    }
+}
---
+++ lopp.java
@@ -0,0 +1,13 @@
+public class lopp {
+    public static void main(String args[]) {
+
+        int bawah = 0;
+        for(bawah=0;bawah<4;);
+            for(int kanan = 1;kanan < 4; kanan++)
+           System.out.print("* ");
+            
+        };
+            
+}
+    
+
