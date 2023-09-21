7e901973c5d9b7bd3f7a6d31484861eb2860fe55
Audyari <audy123ari@gmail.com>
2023-09-21 08:48:11 +0700

    Arrays 8.48

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,6 +1,4 @@
 
-import java.util.Map;
-import java.util.HashMap;
 
 public class Mahasiswa {
     public static void main(String[] args) {
@@ -12,28 +10,3 @@ public class Mahasiswa {
     }
 }
 
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> m = new HashMap<>();
-        
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-        
-        int ans = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
-                ans -= m.get(s.charAt(i));
-            } else {
-                ans += m.get(s.charAt(i));
-            }
-        }
-        
-        return ans;
-    }
-}
