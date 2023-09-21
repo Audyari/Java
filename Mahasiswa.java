de168248baa140d1230f161f31a8c5f8e7c4bb63
Audyari <audy123ari@gmail.com>
2023-09-21 09:20:53 +0700

    StringBuilder 9.20

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -3,10 +3,24 @@
 public class Mahasiswa {
     public static void main(String[] args) {
      
-        int[] test = new int[5];
-        test[0] = 1;
-        System.out.println(test[0]);
+        Solution solution = new Solution(); // Membuat objek Solution
 
+        String word1 = "Hello";
+        String word2 = "World";
+        
+        String merged = solution.mergeAlternately(word1, word2); // Memanggil metode mergeAlternately
+        
+        System.out.println(merged); // Menampilkan hasil penggabungan
     }
 }
 
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+       StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word1.length() + word2.length(); i++) {
+            if(i < word1.length()) sb.append(word1.charAt(i));
+            if(i < word2.length()) sb.append(word2.charAt(i));
+        }
+        return sb.toString();
+    }
+}
